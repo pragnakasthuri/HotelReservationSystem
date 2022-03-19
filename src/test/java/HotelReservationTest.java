c3a21117cef0ca2b328ce5de0f7b6002cbf92a10
pragnakasthruri <kpragna25@gmail.com>
2022-03-19 20:36:07 +0530

    Adding the code for testing the UC2

--- src/test/java/HotelReservationTest.java
+++ src/test/java/HotelReservationTest.java
@@ -1,14 +1,33 @@
+import com.bridgelabz.Hotel;
 import com.bridgelabz.HotelReservationApplication;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class HotelReservationTest {
 
-    @Test
-    public void whenHotelCreation_ShouldReturn3() {
-        HotelReservationApplication.createHotels();
-        int result = HotelReservationApplication.hotelList.size();
-        Assertions.assertEquals(3, result);
+    HotelReservationApplication hotelReservationApplication = null;
+
+    @BeforeEach
+    public void init() {
+        hotelReservationApplication = new HotelReservationApplication();
     }
 
+    @Test
+    public void givenHotelDetails_ShouldReturnTrue() {
+        boolean result = hotelReservationApplication.addHotel(new Hotel("TestHotel", 2, 160, 120));
+        Assertions.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenDate_whenWeekDay_ShouldReturnFalse() {
+        boolean result = hotelReservationApplication.isWeekend("18mar2022");
+        Assertions.assertEquals(result, false);
+    }
+
+    @Test
+    public void givenDate_whenWeekDay_ShouldReturnTrue() {
+        boolean result = hotelReservationApplication.isWeekend("19mar2022");
+        Assertions.assertEquals(result, true);
+    }
 }
