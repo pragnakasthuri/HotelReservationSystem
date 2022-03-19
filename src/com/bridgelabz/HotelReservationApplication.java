8bc0d146ef5b67680569b1d0f66cafe11d438c1e
pragnakasthruri <kpragna25@gmail.com>
2022-03-19 08:45:43 +0530

    Adding the code for Welcome message

---
+++ src/com/bridgelabz/HotelReservationApplication.java
@@ -0,0 +1,18 @@
+package com.bridgelabz;
+
+/**
+ * Creating a HotelReservationApplication to print Welcome message
+ */
+
+public class HotelReservationApplication {
+    /**
+     * Main method to perform modification on Hotel Reservation System
+     * @param args - default java param
+     */
+    public static void main(String[] args) {
+        /**
+         * Printing a welcome message
+         */
+        System.out.println("Welcome to the Hotel Reservation System");
+    }
+}
