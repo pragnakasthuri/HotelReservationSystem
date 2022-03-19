99436bf790f25e8d728fa230be74ba0668ee05ea
pragnakasthruri <kpragna25@gmail.com>
2022-03-19 16:15:06 +0530

    Adding the Test case for UC1

--- src/test/java/HotelReservationTest.java
+++ src/test/java/HotelReservationTest.java
@@ -1,14 +1,22 @@
+import com.bridgelabz.Hotel;
 import com.bridgelabz.HotelReservationApplication;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class HotelReservationTest {
 
+    HotelReservationApplication hotelReservationApplication = null;
+
+    @BeforeEach
+    public void init() {
+        hotelReservationApplication = new HotelReservationApplication();
+    }
+
     @Test
-    public void whenHotelCreation_ShouldReturn3() {
-        HotelReservationApplication.createHotels();
-        int result = HotelReservationApplication.hotelList.size();
-        Assertions.assertEquals(3, result);
+    public void givenHotel_ShouldReturnTrue() {
+        boolean result = hotelReservationApplication.addHotel(new Hotel("TestHotel", 2, 100, 120));
+        Assertions.assertEquals(true, result);
     }
 
 }
